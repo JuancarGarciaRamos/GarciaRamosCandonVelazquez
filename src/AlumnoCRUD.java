e0f62ba225c8728aaea5d6d1bc4cf3244d52b41f
Adrian Candon <adriancandon00@gmail.com>
2023-11-15 12:24:26 +0100

    Añadido metodo bajaAlumno

--- src/AlumnoCRUD.java
+++ src/AlumnoCRUD.java
@@ -62,5 +62,15 @@ public class AlumnoCRUD {
         escribirAlumnos(listaAlumno);
     }
 
+    public void bajaAlumno(String dni) {
+        ArrayList<Alumno> listaAlumno = leerAlumnos();
+        for (int i = 0; i < listaAlumno.size(); i++) {
+            if (listaAlumno.get(i).getDni().equalsIgnoreCase(dni)) {
+                listaAlumno.remove(i);
+            }
+        }
+        escribirAlumnos(listaAlumno);
+    }
+
 
 }
